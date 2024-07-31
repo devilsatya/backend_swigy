369de8bf39402aeafd09e890136d4e18b4790098
srisatya <srisatyasaikumar111@gmail.com>
2024-07-31 14:15:18 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const dotEnv = require('dotenv');
+const mongoose = require('mongoose');
+const vendorRoutes = require('./routes/vendorRoutes');
+const bodyParser = require('body-parser');
+const firmRoutes = require('./routes/firmRoutes');
+const productRoutes = require('./routes/productRoutes');
+const cors = require('cors');
+const path = require('path')
+
+const app = express()
+
+const PORT = process.env.PORT || 4000;
+
+dotEnv.config();
+app.use(cors())
+
+
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => console.log("MongoDB connected successfully!"))
+    .catch((error) => console.log(error))
+//dotEnv.config();
+//srisatyasaikumar070
+//oEU77agQUqpVajwE
+
+
+
+
+app.use(bodyParser.json());
+
+app.use('/vendor', vendorRoutes);
+app.use('/firm', firmRoutes)
+app.use('/product', productRoutes);
+app.use('/uploads', express.static('uploads'));
+
+app.listen(PORT, () => {
+    console.log(`server started and running at ${PORT}`);
+});
+
+app.use('/', (req, res) => {
+    res.send("<h1> Welcome to Swigy");
+})
